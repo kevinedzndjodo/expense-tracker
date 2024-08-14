b1638e57d47d7ef39173e64efb50116180f0bd67
Kevin <iamfalash@gmail.com>
2024-08-14 14:34:16 +0100

    tracker

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import AddNewTransaction from "./components/AddNewTransaction";
+import Balance from "./components/Balance";
+import Header from "./components/Header";
+import IncomeExpense from "./components/IncomeExpense";
+import TransactionHistory from "./components/TransactionHistory";
+import { GlobalProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />{" "}
+        <div className="inc-exp-container">
+          <IncomeExpense />
+        </div>
+      </div>
+      <TransactionHistory />
+      <AddNewTransaction />
+    </GlobalProvider>
+  );
+}
+
+export default App;
